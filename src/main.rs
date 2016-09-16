3c5d74e53464de4c763a9832eceaa9968c4811de
Willem Mali <willem.mali@gmail.com>
2016-09-17 00:59:54 +0200

    basic version with first IUP-based window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+//! Example based on hello0 from http://wiki.call-cc.org/iup-tutor
+
+extern crate iup;
+
+use iup::prelude::*;
+use iup::control::Label;
+
 fn main() {
-    println!("Hello!");
+    
+    iup::with_iup(|| Dialog::new(Label::with_title("Hello, world!")).show()).unwrap()
+
+    
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct Line {
+    start: Point,
+    end: Point,
 }
